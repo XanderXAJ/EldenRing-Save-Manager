7ecea563525354b58863521d73c24d458d235be6
Alex Palmer <XanderXAJ@users.noreply.github.com>
2022-09-11 17:04:31 +0100

    Ensure data cannot be confused with an inline data prefix

--- build-windows.ps1
+++ build-windows.ps1
@@ -1,2 +1,2 @@
-poetry run pyinstaller --onefile --windowed --icon=.\data\icon.ico --add-data=data;data --hidden-import='PIL._tkinter_finder' \
+poetry run pyinstaller --onefile --windowed --icon=.\data\icon.ico --add-data="data;data" --hidden-import='PIL._tkinter_finder' \
 	.\SaveManager.py .\hexedit.py .\stat_progression.py .\itemdata.py .\os_layer.py
