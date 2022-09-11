98b1033840e2baddd8f0cce40a42defa4a5f28cb
Alex Palmer <XanderXAJ@users.noreply.github.com>
2022-09-11 14:25:49 +0100

    Install dependencies as part of the build

--- build-linux.sh
+++ build-linux.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+poetry install
 poetry run pyinstaller --onefile --windowed --icon=./data/icon.ico --add-data=data/:data --hidden-import='PIL._tkinter_finder' \
 	./SaveManager.py ./hexedit.py ./stat_progression.py ./itemdata.py ./os_layer.py
