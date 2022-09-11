a3874c86e46e037c410c99e72184a6666322d63e
Alex Palmer <XanderXAJ@users.noreply.github.com>
2022-09-11 17:49:17 +0100

    Remove pyinstaller spec generation options

--- build-linux.sh
+++ build-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-poetry run pyinstaller --onefile --windowed SaveManager.spec
+poetry run pyinstaller SaveManager.spec
 
 # Copy all data files in to final distribution
 cp -a data dist/data
